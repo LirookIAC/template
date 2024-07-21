df24386317946f4834018641d31640316cc1df2a
abhishek6969 <sunkaleabhishek@gmail.com>
2024-07-21 13:32:13 +0530

    deploy

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "remote-backend"
     storage_account_name = "lirookbackend"
     container_name       = "state-folder"
-    key                  = ""
+    key                  = "test.terraform.tfstate"
   }
 }
 provider "azurerm" {
